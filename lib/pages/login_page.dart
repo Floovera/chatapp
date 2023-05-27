932af6286b1e2871f9b196c73ba06e94fc921fb1
Floovera <r0385588@student.thomasmore.be>
2023-05-27 13:23:15 +0200

    Asset toegevoegd

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,10 +8,30 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("LoginPage")),
-    );
+        body: SingleChildScrollView(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+              key: formKey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      "Groupie",
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text("Login to see what we are talking about",
+                    style: TextStyle(fontSize: 15)),
+                    Image.asset("assets/login.png"),
+                  ]))),
+    ));
   }
 }
