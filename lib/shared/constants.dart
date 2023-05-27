773cfacbeb4612d3e74ce7b7b2c52af72fbf9a1d
Floovera <r0385588@student.thomasmore.be>
2023-05-27 12:53:55 +0200

    Opzet inclus Firebase

---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+class Constants{
+  static String appId = "1:417775417661:web:6609cdc6285506ecbe6d54";
+  static String apiKey = "AIzaSyCaqV1dHShIhtpW63S5H4k5A-a1el1TaYw";
+  static String messagingSenderId = "417775417661";
+  static String projectId = "chatapp-f3500";
+
+}
